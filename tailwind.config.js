964717608df918751fa2a5d936155b232e60f356
Afrndyyy1 <araiz5021@gmail.com>
2022-09-19 14:58:47 +0700

    food tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pop : ['Poppins'],
+      },
+      spacing: {
+        444 : '27rem',
+        364 : '22rem',
+      }
+    },
+  },
+  plugins: [],
+}
